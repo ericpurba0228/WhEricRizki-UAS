4a8947c0f195a8acabb93932b2e22440d60815ec
ericpurba0228 <ericrizkielipelenpurba@gmail.com>
2025-05-17 05:25:12 -1200

    uas selesai

--- lib/data_saya.dart
+++ lib/data_saya.dart
@@ -1,4 +1,4 @@
 class DataSaya {
-   static String nama = "Kudut";
-    static String gambar = "assets/mahasiswa/kudut.png";
+   static String nama = "Eric Rizki Elipelen Purba";
+    static String gambar = "assets/mahasiswa/eric.jpeg";
 }
